a80214fbf20bb03b5ecc4f04b58606ab5ed2303a
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-05-25 17:58:20 +0300

    ttt

--- cli/main.go
+++ cli/main.go
@@ -5,7 +5,7 @@ import (
 	"sort"
 	"time"
 
-	"github.com/urfave/cli"
+	// "github.com/urfave/cli"
 )
 
 const (
@@ -17,37 +17,37 @@ const (
 func Run() {
 	// Phase 1. Get cli options, some validation checks and configure working env
 	// errors from this phase must be paniced with traceback and os.exit(1)
-	app := cli.NewApp()
-	app.Name = NAME
-	app.Version = VERSION
-	app.Compiled = time.Now()
-	app.Authors = []cli.Author{
-		{
-			Name:  "Alexander Gurinov",
-			Email: "alexander.gurinov@gmail.com",
-		},
-		{
-			Name:  "Alexey Yollov",
-			Email: "yollov@me.com",
-		},
-	}
-	app.Usage = USAGE
-	app.Flags = []cli.Flag{
-		cli.BoolFlag{
-			Name:  "debug",
-			Usage: debugFlagUsage,
-		},
-	}
-	app.Commands = []cli.Command{
-		{
-			Name:   "run",
-			Usage:  runCommandUsage,
-			Action: runCommandAction,
-		},
-	}
-	// configure sorting for help
-	sort.Sort(cli.FlagsByName(app.Flags))
-	sort.Sort(cli.CommandsByName(app.Commands))
-	// run
-	app.Run(os.Args)
+	// app := cli.NewApp()
+	// app.Name = NAME
+	// app.Version = VERSION
+	// app.Compiled = time.Now()
+	// app.Authors = []cli.Author{
+	// 	{
+	// 		Name:  "Alexander Gurinov",
+	// 		Email: "alexander.gurinov@gmail.com",
+	// 	},
+	// 	{
+	// 		Name:  "Alexey Yollov",
+	// 		Email: "yollov@me.com",
+	// 	},
+	// }
+	// app.Usage = USAGE
+	// app.Flags = []cli.Flag{
+	// 	cli.BoolFlag{
+	// 		Name:  "debug",
+	// 		Usage: debugFlagUsage,
+	// 	},
+	// }
+	// app.Commands = []cli.Command{
+	// 	{
+	// 		Name:   "run",
+	// 		Usage:  runCommandUsage,
+	// 		Action: runCommandAction,
+	// 	},
+	// }
+	// // configure sorting for help
+	// sort.Sort(cli.FlagsByName(app.Flags))
+	// sort.Sort(cli.CommandsByName(app.Commands))
+	// // run
+	// app.Run(os.Args)
 }
