a25dd20133e55bd424e92fe76db43935954f3656
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-05-25 18:03:13 +0300

    fixes

--- cli/actions.go
+++ cli/actions.go
@@ -2,17 +2,9 @@ package cli
 
 import (
 	"fmt"
-	"net"
-	"os"
-	"os/exec"
-	"os/user"
-	"runtime"
-	"text/template"
 
 	"github.com/urfave/cli"
-
 	"github.com/agurinov/dnskek/log"
-	"github.com/agurinov/dnskek/server"
 )
 
 var (
