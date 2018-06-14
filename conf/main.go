28d0e94437e6a31ee50b3ee6872dfbcd51ce6e3d
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-06-14 15:53:14 +0300

    fix conf load from file

--- conf/main.go
+++ conf/main.go
@@ -10,15 +10,15 @@ func LoadFile(filename string) (*Router, error) {
 	// Try to read config file
 	YAML, err := ioutil.ReadFile(filename)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// config file loaded, try to parse yaml into router
 	var router Router
 
 	if err := yaml.Unmarshal(YAML, &router); err != nil {
-		return err
+		return nil, err
 	}
 
-	return &router
+	return &router, nil
 }
