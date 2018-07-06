0b7dc5f4505d3291d84b1c2c073e00c506548581
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-07-06 15:11:01 +0300

    dont repeat yourself

--- .circleci/scripts/run.sh
+++ .circleci/scripts/run.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -e
 
-go get -v -d ./...
-GOOS=linux GOARCH=amd64 go build \
-	-v \
-	-ldflags "-X main.VERSION=local -X main.TIMESTAMP=`date +%s`" \
-	-o /go/bin/base-Linux-x86_64
+apk add --update git
+
+./build.sh
 
 /go/bin/base-Linux-x86_64 run tcp
