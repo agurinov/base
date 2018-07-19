e05de90b2380ae7bdba8b2f6e431e2aa568ba757
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-07-19 14:02:02 +0300

    add bin volume and fix build

--- .circleci/scripts/build.sh
+++ .circleci/scripts/build.sh
@@ -2,7 +2,18 @@
 set -e
 
 go get -v -d ./...
+
+VERSION='LOCAL'
+TIMESTAMP=`date +%s`
+
+# linux
 GOOS=linux GOARCH=amd64 go build \
 	-v \
-	-ldflags "-X main.VERSION=LOCAL -X main.TIMESTAMP=`date +%s`" \
+	-ldflags "-X main.VERSION=${VERSION} -X main.TIMESTAMP=${TIMESTAMP}" \
 	-o /go/bin/base-Linux-x86_64
+
+# macos
+GOOS=darwin GOARCH=amd64 go build \
+	-v \
+	-ldflags "-X main.VERSION=${VERSION} -X main.TIMESTAMP=${TIMESTAMP}" \
+	-o /go/bin/base-Darwin-x86_64
