66180ca5adf1625e5651bf4da8468815b6dc5d0b
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-08-01 15:06:28 +0300

    fix

--- cli/log.go
+++ cli/log.go
@@ -9,7 +9,7 @@ import (
 func StartupLog(version string, compiled time.Time) {
 	log.Infof("************************************************************")
 	log.Infof("Boomfunc base version:\t%s", log.Wrap(version, log.Bold))
-	log.Infof("Boomfunc compilation time:\t%s", log.Wrap(compiled.String(), log.Bold))
+	log.Infof("Boomfunc compilation time:\t%s", log.Wrap(compiled, log.Bold))
 	log.Infof("************************************************************")
 	log.Infof("")
 }
