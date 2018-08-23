1bcfa91172526ee885a8aac7ad55270a02bb13fa
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-08-23 15:37:21 +0300

    Close #6, but with some future TODO’s

--- tools/net_test.go
+++ tools/net_test.go
@@ -2,6 +2,7 @@ package tools
 
 import (
 	"fmt"
+	"net"
 	"testing"
 )
 
@@ -28,6 +29,55 @@ func TestIsBot(t *testing.T) {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
 			if isBot := IsBot(tt.ua); isBot != tt.bot {
 				t.Errorf("Expected \"%t\", got \"%t\"", tt.bot, isBot)
+			}
+		})
+	}
+}
+
+func TestIsPrivateIP(t *testing.T) {
+	tableTests := []struct {
+		ip      net.IP
+		private bool
+	}{
+		{net.ParseIP(""), false},
+		{net.ParseIP("127.0.2.43"), true},
+		{net.ParseIP("10.1.0.54"), true},
+		{net.ParseIP("10.66.63.98"), true},
+		{net.ParseIP("172.16.4.230"), true},
+		{net.ParseIP("192.168.12.54"), true},
+	}
+
+	for i, tt := range tableTests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			if private := isPrivateIP(tt.ip); private != tt.private {
+				t.Errorf("Expected \"%t\", got \"%t\"", tt.private, private)
+			}
+		})
+	}
+}
+
+func TestHeadersIP(t *testing.T) {
+	tableTests := []struct {
+		headers []string
+		ip      net.IP
+	}{
+		// real examples
+		{[]string{"5.5.5.5"}, net.ParseIP("5.5.5.5")},
+		{[]string{"46.138.53.10, 66.249.93.22"}, net.ParseIP("46.138.53.10")},
+		{[]string{"217.138.39.59"}, net.ParseIP("217.138.39.59")},
+		{[]string{"10.66.63.98, 207.47.45.161"}, net.ParseIP("207.47.45.161")},
+		{[]string{"108.48.6.2, 66.249.83.194"}, net.ParseIP("108.48.6.2")},
+		{[]string{"108.48.6.2, 66.249.83.43"}, net.ParseIP("108.48.6.2")},
+		// fake
+		{[]string{"192.168.0.1"}, nil},
+		{[]string{"192.168.0.1, 10.1.0.54"}, nil},
+		{[]string{"192.168.0.1, 10.1.0.54, 127.0.2.43"}, nil},
+	}
+
+	for i, tt := range tableTests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			if ip := headersIP(tt.headers...); !ip.Equal(tt.ip) {
+				t.Errorf("Expected %q, got %q", tt.ip, ip)
 			}
 		})
 	}
