bbfacd8b3d60d8d579fed2f02db163080ddb7eed
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-10-12 18:49:16 +0300

    add ability to load yaml (config) from different sources

--- conf/main.go
+++ conf/main.go
@@ -1,24 +1,46 @@
 package conf
 
 import (
-	"io/ioutil"
+	"io"
+	"net/http"
+	"os"
 
 	"gopkg.in/yaml.v2"
 )
 
-func LoadFile(filename string) (*Router, error) {
-	// Try to read config file
-	YAML, err := ioutil.ReadFile(filename)
-	if err != nil {
-		return nil, err
-	}
+func loadYAML(r io.Reader) (*Router, error) {
+	// Try to read raw yaml
+	decoder := yaml.NewDecoder(r)
+	decoder.SetStrict(false)
 
-	// config file loaded, try to parse yaml into router
 	var router Router
-
-	if err := yaml.Unmarshal(YAML, &router); err != nil {
+	if err := decoder.Decode(&router); err != nil {
 		return nil, err
 	}
 
 	return &router, nil
+}
+
+// LoadLocalFile loads YAML config from local file system by name
+func LoadLocalFile(name string) (*Router, error) {
+	// try to open file and get io.Reader
+	f, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+
+	// parse yaml
+	return loadYAML(f)
+}
+
+// LoadExternalFile loads YAML config from http
+func LoadExternalFile(url string) (*Router, error) {
+	// try to open file and get io.Reader
+	r, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	// parse yaml
+	return loadYAML(r.Body)
 }
