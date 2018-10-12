226a3f038627a028aca841e5fad788830e7f49f7
Alexander Gurinov <alexander.gurinov@gmail.com>
2018-10-12 18:52:59 +0300

    fix test if no cache exists

--- .ci/scripts/test.sh
+++ .ci/scripts/test.sh
@@ -2,7 +2,7 @@
 set -ex
 
 go get -d -t ./...
-go clean -testcache
+go clean -testcache || true
 go test -race -cover ./...
 # go test -race -cover ./tools/chronometer
 # go test -race -cover tools/context_test.go
